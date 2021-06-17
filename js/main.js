8b72edd409b664e4e75e7afaa39af95d1b07063a
JosephThomas <josephthomas88m@gmail.com>
2021-06-17 10:51:51 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -1,46 +1,4 @@
 $(document).ready(function () {
-
-
-    let $btns = $('.project-area .button-group button');
-
-
-    $btns.click(function (e) {
-
-        $('.project-area .button-group button').removeClass('active');
-        e.target.classList.add('active');
-
-        let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
-            filter: selector
-        });
-
-        return false;
-    })
-
-    $('.project-area .button-group #btn1').trigger('click');
-
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
-    });
-
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
-        }
-    })
-
     // sticky navigation menu
 
     let nav_offset_top = $('.header_area').height() + 50;
